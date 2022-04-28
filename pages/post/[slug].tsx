36492c3f19d9831ee9932798a552ab9cea8e39d4
Abdulrahman Hamad <wadaln3ma@gmail.com>
2022-04-28 06:17:17 +0300

    Show Comments in post page

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -169,6 +169,23 @@ const Post : React.FC = ({ post } : Props)=>{
           </form>
         )}
 
+      {/* Comments */}
+        <div className="my-10 mx-auto flex max-w-2xl flex-col space-y-2 p-10 shadow  shadow-yellow-500">
+          <h3 className="text-4xl">Comments</h3>
+          <hr className="pb-2" />
+
+          {post.comments.map((comment) => {
+            return (
+              <div key={comment._id}>
+                <p>
+                  <span className="text-yellow-500">{comment.name}: </span>
+                  {comment.comment}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+
     </div>
 )}
 
