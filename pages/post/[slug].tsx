c225dca2381bdef6f65a7fd71c908761b0dd830f
Abdulrahman Hamad <wadaln3ma@gmail.com>
2022-04-28 06:06:35 +0300

    Implement posting comments section
    now you can comment on post.

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,14 +1,45 @@
+import { useState } from 'react'
 import { GetStaticProps } from 'next'
 import Header from '../../components/Header'
 import { sanityClient, urlFor } from '../../sanity'
 import { Post } from '../../typing'
 import PortableText from 'react-portable-text'
+import { useForm, SubmitHandler } from 'react-hook-form'
+
+interface IFormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
 
 interface Props {
   post: Post
 }
 
 const Post : React.FC = ({ post } : Props)=>{
+
+  const [submitted, setSubmitted] = useState(false)
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    await fetch('/api/createComment', {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        setSubmitted(true)
+      })
+      .catch((err) => {
+        setSubmitted(false)
+      })
+  }
+
   return (
     <div>
       <Header />
@@ -62,6 +93,81 @@ const Post : React.FC = ({ post } : Props)=>{
             />
           </div>
         </article>
+      
+      <hr className="my-5 mx-auto max-w-lg border border-yellow-500" />
+
+        {submitted ? (
+          <div className="my-10 mx-auto flex max-w-2xl flex-col bg-yellow-500 p-10 text-white">
+            <h3>Thank you for submitting your comment</h3>
+            <p>Once it has been approved, it will appear below! </p>
+          </div>
+        ) : (
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="mx-auto mb-10 flex max-w-2xl flex-col p-5"
+          >
+            <h3 className="text-sm text-yellow-500">Enjoy this article?</h3>
+            <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+            <hr className="mt-2 py-3" />
+
+            <input
+              {...register('_id')}
+              type="hidden"
+              name="_id"
+              value={post._id}
+            />
+
+            <label className="mb-5 block">
+              <span className="text-gray-700">Name</span>
+              <input
+                {...register('name', { required: true })}
+                className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+                placeholder="name"
+                type="text"
+              />
+            </label>
+            <label className="mb-5 block">
+              <span className="text-gray-700">Email</span>
+              <input
+                {...register('email', { required: true })}
+                className="form-input mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+                placeholder="email"
+                type="email"
+              />
+            </label>
+            <label className="mb-5 block">
+              <span className="text-gray-700">Comment</span>
+              <textarea
+                {...register('comment', { required: true })}
+                className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
+                placeholder="comment"
+                rows={8}
+              />
+            </label>
+            <div className="flex flex-col">
+              {errors.name && (
+                <span className="text-red-500">
+                  - The Name Field is required
+                </span>
+              )}
+              {errors.comment && (
+                <span className="text-red-500">
+                  - The Comment Field is required
+                </span>
+              )}
+              {errors.email && (
+                <span className="text-red-500">
+                  - The Email Field is required
+                </span>
+              )}
+            </div>
+
+            <input
+              type="submit"
+              className="focus:shadow-outline cursor-pointer rounded bg-yellow-500  px-4 py-2 font-bold text-white shadow hover:bg-yellow-400"
+            />
+          </form>
+        )}
 
     </div>
 )}
